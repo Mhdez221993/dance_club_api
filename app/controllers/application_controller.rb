8fb023a37e14d23c3ddb77bf1c9377177b5a19a3
Moises <mhdezcoronado@gmail.com>
2022-03-28 18:49:20 +0200

    Add authorization before any request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  # include RackSessionFixController
+  # include ActionController::RequestForgeryProtection
+
+  # protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+  end
 end
