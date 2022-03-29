9ce9498f1b8383d459dcddf9fd731c9641cf5840
Moises <mhdezcoronado@gmail.com>
2022-03-29 22:11:32 +0200

    Validate precence of email, password & name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_many :items
   has_many :reservations
 
+  validates_presence_of :password, :email, :name
+  validates_length_of :password, minimum: 6, on: :create
+
   def admin?
     role == 'admin'
   end
