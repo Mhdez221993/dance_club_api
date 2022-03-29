28bc78f8dccc563d45d6c274ef00b2bad1430625
Moises <mhdezcoronado@gmail.com>
2022-03-29 15:15:18 +0200

    Create reservation api end-points

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users,
-             controllers: {
-                 sessions: 'users/sessions',
-                 registrations: 'users/registrations'
-             }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   namespace :api do
+    resources :reservations
     resources :items
   end
 end
