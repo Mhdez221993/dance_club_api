d17d0a9db22e0a88d2354b52c9b497390294583c
Moises <mhdezcoronado@gmail.com>
2022-03-30 15:55:21 +0200

    Test reservation controller

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'devise/jwt/test_helpers'
 
-describe 'Users controller', type: :request do
+describe 'Items controller', type: :request do
   let!(:user) { User.create name: 'Test', email: 'testing@test.com', password: '123456', role: 'admin' }
   let!(:item) { user.items.create name: "Dancing flow", description: "Improvise dance", finance: 129, option: 249, total: 9.299, duration: 24, apr: 2.9 }
   let!(:items) { Item.all }
