e692f42733685956a16ed9405e8aa1071c7f7240
Moises <mhdezcoronado@gmail.com>
2022-03-31 13:21:06 +0200

    Deploy to heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3000', 'comic_dance_club_back-end.herokuapp.com'
 
     resource '*',
       headers: :any,
