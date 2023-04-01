5e24910345d610c0783569661e9a964215e12d63
Moises <mhdezcoronado@gmail.com>
2023-04-01 14:00:01 +0200

    Update application config

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module ComicDanceClubBackEnd
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.assets.precompile += %w( *.js *.css )
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
